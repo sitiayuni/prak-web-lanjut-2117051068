8f4b5ae53219fa4c880c543192766b92f8729401
sitiayuni <siti.ayuni21@students.unila.ac.id>
2023-09-26 14:37:45 +0700

    Create User Profile-Controller

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <img src="<?=base_url("assets/img/foto_Siti Ayuni.jpg")?>" alt="" width="200px">
+    <img ref="stylesheet" href="<?=base_url("assets/css/style.css")?>">
 </head>
 
 <body>
