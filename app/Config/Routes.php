ec681acfee23abb64fe290877805631082f372ba
sitiayuni <siti.ayuni21@students.unila.ac.id>
2023-09-26 10:41:31 +0700

    Tambah halaman Profile

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,10 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\Home;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/profile/(:any)/(:any)/(:any)', [Home::class, 'profile']);
